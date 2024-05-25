12fd020a2375edd4466b744347a3bda15fb01ee2
JanSz37 <141222420+JanSz37@users.noreply.github.com>
2024-05-25 14:57:44 +0200

    Add files via upload

---
+++ tracking_done/GameClass.R
@@ -0,0 +1,155 @@
+library(igraph)
+library(R6)
+
+Game <- R6Class(
+  "Game",
+  
+  public = list(
+    player1_name = NULL,
+    player2_name = NULL,
+    edges = NULL,
+    edge_labels = NULL,
+    node_data = NULL,
+    start_game_clicked = FALSE,
+    
+    initialize = function(player1_name, player2_name, edges = c(), edge_labels = c(), node_data = list(), start_game_clicked = FALSE) {
+      self$player1_name <- player1_name
+      self$player2_name <- player2_name
+      self$edges <- edges
+      self$edge_labels <- edge_labels
+      self$node_data <- node_data
+      self$start_game_clicked <- start_game_clicked
+    },
+    
+    addDecisionAndEdge = function(root_node_index, decision, is_last_node = FALSE, points_Player1 = 0, points_Player2 = 0) {
+      next_letter <- if (length(self$edges) == 0) "B" else LETTERS[max(as.numeric(factor(substr(self$edges, 1, 1), levels = LETTERS))) + 1]
+      if (is_last_node) {
+        last_node <- paste(next_letter, ". (", points_Player1, " | ", points_Player2, ")", sep = "")
+        self$edges <- c(self$edges, root_node_index, last_node)
+        self$node_data[[last_node]] <- list(points_Player1 = points_Player1, points_Player2 = points_Player2, decision_path = c())
+      } else {
+        new_node <- next_letter
+        self$edges <- c(self$edges, root_node_index, new_node)
+        self$node_data[[new_node]] <- list(points_Player1 = NULL, points_Player2 = NULL, decision_path = c())
+      }
+      self$edge_labels <- c(self$edge_labels, decision)
+    },
+    
+    getPathsToPayoffs = function() {
+      leaf_nodes <- names(self$node_data)[sapply(self$node_data, function(x) !is.null(x$points_Player1) && !is.null(x$points_Player2))]
+      paths_to_payoffs <- list()
+      
+      for (leaf in leaf_nodes) {
+        path <- self$backtrackPath(leaf)
+        payoff_key <- paste("(", self$node_data[[leaf]]$points_Player1, ", ", self$node_data[[leaf]]$points_Player2, ")", sep = "")
+        if (!payoff_key %in% names(paths_to_payoffs)) {
+          paths_to_payoffs[[payoff_key]] <- list()
+        }
+        paths_to_payoffs[[payoff_key]] <- c(paths_to_payoffs[[payoff_key]], list(path))
+      }
+      
+      return(paths_to_payoffs)
+    },
+    
+    backtrackPath = function(leaf_node) {
+      path <- c()
+      current_node <- leaf_node
+      
+      while (current_node != "A") {
+        edge_index <- which(self$edges == current_node)
+        if (length(edge_index) == 0) break
+        edge_index <- edge_index[1] - 1
+        path <- c(self$edge_labels[ceiling(edge_index / 2)], path)
+        current_node <- self$edges[edge_index]
+      }
+      
+      return(path)
+    },
+    
+    plotTree = function() {
+      if (!is.null(self$edges) && length(self$edges) > 0) {
+        g <- graph(self$edges, directed = TRUE)
+        
+        distances_from_root <- distances(g, v = "A", to = V(g), mode = "out")
+        tree_depth <- max(distances_from_root[distances_from_root != Inf]) + 1
+        
+        if (!is.null(tree_depth)) {
+          tree_layout <- layout_as_tree(g, root = "A")
+          
+          y_axis_labels <- if (tree_depth %% 2 == 0) {
+            rep(c(self$player2_name, self$player1_name), length.out = tree_depth)
+          } else {
+            rep(c(self$player1_name, self$player2_name), length.out = tree_depth)
+          }
+          
+          plot.new()
+          padding <- 0.5 # Adjust the padding value as needed
+          plot.window(xlim = range(tree_layout[, 1]), ylim = c(-padding, tree_depth - 1 + padding))
+          axis(2, at = seq(0, tree_depth - 1, by = 1), labels = y_axis_labels)
+          
+          par(new = TRUE)
+          plot(g, layout = tree_layout,
+               vertex.color = "#A7C957",
+               vertex.size = 40,
+               vertex.label.color = "black",
+               edge.arrow.size = 0.5,
+               edge.color = "black",
+               edge.label = self$edge_labels,
+               edge.label.color = "black",
+               rescale = FALSE, 
+               xlim = range(tree_layout[, 1]), 
+               ylim = c(-padding, tree_depth - 1 + padding))
+          
+          mtext("Decision", side = 2, line = 3)
+        }
+      }
+    },
+    
+    getEdges = function() {
+      return(self$edges)
+    }
+  )
+)
+
+
+
+GameVectors <- R6Class(
+  "GameVectors",
+  inherit = Game,
+  
+  public = list(
+    dec1 = NULL,
+    dec2 = NULL,
+    payouts1 = NULL,
+    payouts2 = NULL,
+    i = 1,
+    
+    initialize = function(player1_name, player2_name, edges = c(), edge_labels = c(), node_data = list(), start_game_clicked = FALSE, dec1 = c(), dec2 = c(), payouts1 = c(), payouts2 = c()) {
+      super$initialize(player1_name, player2_name, edges, edge_labels, node_data, start_game_clicked)
+      self$dec1 <- dec1
+      self$dec2 <- dec2
+      self$payouts1 <- payouts1
+      self$payouts2 <- payouts2
+    },
+    
+    addDecisionAndEdge_Vector = function(root_node_index, decision, is_last_node = FALSE, points_Player1 = 0, points_Player2 = 0) {
+      super$addDecisionAndEdge(root_node_index, decision, is_last_node, points_Player1, points_Player2)
+      
+      if (self$i %% 2 == 1) {
+       self$dec1 <- c(self$dec1, decision)
+      } else {
+       self$dec2 <- c(self$dec2, decision)
+      }
+      self$i <- self$i + 1
+      
+      if (is_last_node) {
+        self$payouts1 <- c(self$payouts1, points_Player1)
+        self$payouts2 <- c(self$payouts2, points_Player2)
+      }
+    },
+    
+    correlatePayoffsWithPaths = function() {
+      return(self$getPathsToPayoffs())
+    }
+  )
+)
---
+++ tracking_done/shinyapp.R
@@ -0,0 +1,203 @@
+library(shiny)
+library(igraph)
+library(R6)
+source("GameClass.R")
+
+# init the game instance
+game_instance <- reactiveVal(NULL)
+game_vectors <- reactiveVal(NULL)
+test <- reactiveValues(value = list())
+
+#############################################
+#                                           #
+#                 FRONTEND                  #
+#                                           #
+#############################################
+ui <- fluidPage(
+  titlePanel("Strategies in Game Theory"),
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput("playerInputs"),
+      uiOutput("startGameButton"),
+      uiOutput("addNodeInputs"),
+      actionButton("printPayoffPaths", "Print Payoff Paths", style = "background-color: #BC4749; color: white;")
+    ),
+    mainPanel(
+      uiOutput("graph_and_heading")
+    )
+  ),
+  fluidRow(
+    column(12,
+           align = "center",
+           style = "margin-top: 20px;",
+           div(style = "background-color: #f8f9fa; padding: 10px;",
+               div(style = "display: inline-block; margin-right: 10px;", uiOutput("newGameButton")),
+               div(style = "display: inline-block; margin-right: 10px;", actionButton("aboutButton", "About")),
+               div(style = "display: inline-block;", actionButton("createdByButton", "Created By"))
+           )
+    )
+  )
+)
+
+
+#############################################
+#                                           #
+#                 BACKEND                   #
+#                                           #
+#############################################
+server <- function(input, output, session) {
+  
+  game_instance <- reactiveVal(NULL)
+  game_vectors <- reactiveVal(NULL)
+  error_message <- reactiveVal("")
+  
+  output$playerInputs <- renderUI({
+    if (is.null(game_instance()) || !game_instance()$start_game_clicked) {
+      tagList(
+        textInput("Player1_Name", "Player 1 Name:", value = ""),
+        textInput("Player2_Name", "Player 2 Name:", value = ""),
+        uiOutput("errorMessage")
+      )
+    }
+  })
+  
+  output$startGameButton <- renderUI({
+    if (is.null(game_instance()) || !game_instance()$start_game_clicked) {
+      actionButton("startGame", "Create Game", style = "background-color: #BC4749; color: white;")
+    }
+  })
+  
+  output$errorMessage <- renderUI({
+    if (!is.null(error_message()) && error_message() != "") {
+      div(style = "color: red;", error_message())
+    }
+  })
+  
+  observeEvent(input$startGame, {
+    if (is.null(input$Player1_Name) || input$Player1_Name == "" || 
+        is.null(input$Player2_Name) || input$Player2_Name == "") {
+      error_message("Player names cannot be empty. Please enter names for both players.")
+    } else if (nchar(input$Player1_Name) > 16 || nchar(input$Player2_Name) > 16) {
+      error_message("Player names cannot be longer than 16 characters. Please enter shorter names.")
+    } else if (tolower(input$Player1_Name) == tolower(input$Player2_Name)) {
+      error_message("Player names cannot be the same. Please enter different names for both players.")
+    } else {
+      error_message("")
+      game_instance(Game$new(input$Player1_Name, input$Player2_Name, start_game_clicked = TRUE))
+      game_vectors(GameVectors$new(input$Player1_Name, input$Player2_Name))
+    }
+  })
+  
+  observe({
+    if (!is.null(game_instance()) && game_instance()$start_game_clicked) {
+      output$addNodeInputs <- renderUI({
+        tagList(
+          actionButton("addNode", "Add Node"),
+          checkboxInput("lastNodeToggle", "Last Node", value = FALSE),
+          textInput("rootNodeIndex", "Root Node Index:", value = ""),
+          textInput("decision", "Decision:", value = ""),
+          conditionalPanel(
+            condition = "input.lastNodeToggle == true",
+            numericInput("points_Player1", paste("Points for", game_instance()$player1_name, ":"), value = 0),
+            numericInput("points_Player2", paste("Points for", game_instance()$player2_name, ":"), value = 0)
+          )
+        )
+      })
+    } else {
+      output$addNodeInputs <- NULL
+    }
+  })
+  
+  observeEvent(input$addNode, {
+    req(game_instance())
+    existing_edges <- game_instance()$getEdges()
+    
+    # Check if the entered rootNodeIndex exists in the existing edges
+    if (length(existing_edges) > 0 && !input$rootNodeIndex %in% unlist(existing_edges)) {
+      error_message("Please provide an existing Node Index")
+    } else {
+      # If the rootNodeIndex is valid, proceed to add the node
+      game_instance()$addDecisionAndEdge(
+        input$rootNodeIndex, 
+        input$decision, 
+        input$lastNodeToggle, 
+        input$points_Player1, 
+        input$points_Player2
+      )
+      
+      game_vectors()$addDecisionAndEdge_Vector( 
+        input$rootNodeIndex, 
+        input$decision, 
+        input$lastNodeToggle, 
+        input$points_Player1, 
+        input$points_Player2
+      )
+      
+      updateTextInput(session, "rootNodeIndex", value = "")
+      updateTextInput(session, "decision", value = "")
+      updateCheckboxInput(session, "lastNodeToggle", value = FALSE)
+      if (input$lastNodeToggle) {
+        updateNumericInput(session, "points_Player1", value = 0)
+        updateNumericInput(session, "points_Player2", value = 0)
+      }
+      
+      # render plot
+      output$network_plot <- renderPlot({
+        req(game_instance())
+        game_instance()$plotTree() 
+      })
+    }
+  })
+  
+  observeEvent(input$newGame, {
+    game_instance(NULL)
+    game_vectors(NULL)
+    updateTextInput(session, "Player1_Name", value = "")
+    updateTextInput(session, "Player2_Name", value = "")
+    output$addNodeInputs <- NULL  # Hide Add Node inputs
+    output$network_plot <- NULL   # Hide network plot
+    
+    # Clear the error message when starting a new game
+    error_message("")
+  })
+  
+  output$newGameButton <- renderUI({
+    if (!is.null(game_instance()) && game_instance()$start_game_clicked) {
+      actionButton("newGame", "Restart Game", style = "background-color: #BC4749; color: white;")
+    } else {
+      NULL
+    }
+  })
+  
+  output$graph_and_heading <- renderUI({
+    tagList(
+      plotOutput("network_plot")
+    )
+  })
+  
+  observeEvent(input$aboutButton, {
+    showModal(modalDialog(
+      title = "About",
+      tags$iframe(src = "about.html", width = "100%", height = "600px", frameborder = 0),
+      easyClose = TRUE,
+      footer = NULL
+    ))
+  })
+  
+  observeEvent(input$createdByButton, {
+    showModal(modalDialog(
+      title = "Created By",
+      tags$iframe(src = "createdby.html", width = "100%", height = "600px", frameborder = 0),
+      easyClose = TRUE,
+      footer = NULL
+    ))
+  })
+  
+  observeEvent(input$printPayoffPaths, {
+    req(game_instance())
+    payoff_paths <- game_instance()$getPathsToPayoffs()
+    print(payoff_paths)
+  })
+}
+
+shinyApp(ui = ui, server = server)
