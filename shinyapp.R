e54136f5c55507e7218b0b3aaade56056158e26f
JanSz37 <141222420+JanSz37@users.noreply.github.com>
2024-05-27 13:18:10 +0200

    UI rozjechane, do poprawy

--- shinyapp.R
+++ shinyapp.R
@@ -1,11 +1,14 @@
 library(shiny)
 library(igraph)
 library(R6)
+
+# Source the GameClass.R file
 source("GameClass.R")
 
-# init the game instance
+# Initialize the game instance
 game_instance <- reactiveVal(NULL)
 game_vectors <- reactiveVal(NULL)
+error_message <- reactiveVal("")
 
 #############################################
 #                                           #
@@ -18,177 +21,92 @@ ui <- fluidPage(
     sidebarPanel(
       uiOutput("playerInputs"),
       uiOutput("startGameButton"),
-      uiOutput("addNodeInputs")
+      uiOutput("addNodeInputs"),
+      uiOutput("addNodeButton"),
+      uiOutput("performBackwardInductionButton")
     ),
     mainPanel(
-      uiOutput("graph_and_heading")
-    )
-  ),
-  fluidRow(
-    column(12,
-           align = "center",
-           style = "margin-top: 20px;",
-           div(style = "background-color: #f8f9fa; padding: 10px;",
-               div(style = "display: inline-block; margin-right: 10px;", uiOutput("newGameButton")),
-               div(style = "display: inline-block; margin-right: 10px;", actionButton("aboutButton", "About")),
-               div(style = "display: inline-block;", actionButton("createdByButton", "Created By"))
-           )
+      plotOutput("treePlot"),
+      verbatimTextOutput("errorMessage"),
+      verbatimTextOutput("optimalPaths")
     )
   )
 )
 
 #############################################
 #                                           #
-#                 BACKEND                   #
+#               SERVER LOGIC                #
 #                                           #
 #############################################
 server <- function(input, output, session) {
-  
-  game_instance <- reactiveVal(NULL)
-  game_vectors <- reactiveVal(NULL)
-  error_message <- reactiveVal("")
-  
   output$playerInputs <- renderUI({
-    if (is.null(game_instance()) || !game_instance()$start_game_clicked) {
-      tagList(
-        textInput("Player1_Name", "Player 1 Name:", value = ""),
-        textInput("Player2_Name", "Player 2 Name:", value = ""),
-        uiOutput("errorMessage")
-      )
-    }
-  })
-  
-  output$startGameButton <- renderUI({
-    if (is.null(game_instance()) || !game_instance()$start_game_clicked) {
-      actionButton("startGame", "Create Game", style = "background-color: #BC4749; color: white;")
-    }
-  })
-  
-  output$errorMessage <- renderUI({
-    if (!is.null(error_message()) && error_message() != "") {
-      div(style = "color: red;", error_message())
-    }
-  })
-  
-  observeEvent(input$startGame, {
-    if (is.null(input$Player1_Name) || input$Player1_Name == "" || 
-        is.null(input$Player2_Name) || input$Player2_Name == "") {
-      error_message("Player names cannot be empty. Please enter names for both players.")
-    } else if (nchar(input$Player1_Name) > 16 || nchar(input$Player2_Name) > 16) {
-      error_message("Player names cannot be longer than 16 characters. Please enter shorter names.")
-    } else if (tolower(input$Player1_Name) == tolower(input$Player2_Name)) {
-      error_message("Player names cannot be the same. Please enter different names for both players.")
-    } else {
-      error_message("")
-      game_instance(Game$new(input$Player1_Name, input$Player2_Name, start_game_clicked = TRUE))
-      game_vectors(GameVectors$new(input$Player1_Name, input$Player2_Name))
-    }
-  })
-  
-  
-  observe({
-    if (!is.null(game_instance()) && game_instance()$start_game_clicked) {
-      output$addNodeInputs <- renderUI({
-        tagList(
-          actionButton("addNode", "Add Node"),
-          checkboxInput("lastNodeToggle", "Last Node", value = FALSE),
-          textInput("rootNodeIndex", "Root Node Index:", value = ""),
-          textInput("decision", "Decision:", value = ""),
-          conditionalPanel(
-            condition = "input.lastNodeToggle == true",
-            numericInput("points_Player1", paste("Points for", game_instance()$player1_name, ":"), value = 0),
-            numericInput("points_Player2", paste("Points for", game_instance()$player2_name, ":"), value = 0)
-          )
-        )
-      })
-    } else {
-      output$addNodeInputs <- NULL
-    }
-  })
-  
-  observeEvent(input$addNode, {
-    req(game_instance())
-    existing_edges <- game_instance()$getEdges()
-    
-    # Check if the entered rootNodeIndex exists in the existing edges
-    if (length(existing_edges) > 0 && !input$rootNodeIndex %in% unlist(existing_edges)) {
-      error_message("Please provide an existing Node Index")
-    } else {
-      # If the rootNodeIndex is valid, proceed to add the node
-      game_instance()$addDecisionAndEdge(
-        input$rootNodeIndex, 
-        input$decision, 
-        input$lastNodeToggle, 
-        input$points_Player1, 
-        input$points_Player2
-      )
-      
-      game_vectors()$addDecisionAndEdge_Vector( 
-        input$rootNodeIndex, 
-        input$decision, 
-        input$lastNodeToggle, 
-        input$points_Player1, 
-        input$points_Player2
-      )
-      
-      updateTextInput(session, "rootNodeIndex", value = "")
-      updateTextInput(session, "decision", value = "")
-      updateCheckboxInput(session, "lastNodeToggle", value = FALSE)
-      if (input$lastNodeToggle) {
-        updateNumericInput(session, "points_Player1", value = 0)
-        updateNumericInput(session, "points_Player2", value = 0)
-      }
-      
-      # render plot
-      output$network_plot <- renderPlot({
-        req(game_instance())
-        game_instance()$plotTree() 
-      })
-    }
-  })
-  
-  observeEvent(input$newGame, {
-    game_instance(NULL)
-    game_vectors(NULL)
-    updateTextInput(session, "Player1_Name", value = "")
-    updateTextInput(session, "Player2_Name", value = "")
-    output$addNodeInputs <- NULL  # Hide Add Node inputs
-    output$network_plot <- NULL   # Hide network plot
-    
-    # Clear the error message when starting a new game
-    error_message("")
-  })
-  
-  output$newGameButton <- renderUI({
-    if (!is.null(game_instance()) && game_instance()$start_game_clicked) {
-      actionButton("newGame", "Restart Game", style = "background-color: #BC4749; color: white;")
-    } else {
-      NULL
-    }
-  })
-  
-  output$graph_and_heading <- renderUI({
     tagList(
-      plotOutput("network_plot")
+      textInput("player1", "Player 1 Name:", value = "Player 1"),
+      textInput("player2", "Player 2 Name:", value = "Player 2")
     )
   })
   
-  observeEvent(input$aboutButton, {
-    showModal(modalDialog(
-      title = "About",
-      tags$iframe(src = "about.html", width = "100%", height = "600px", frameborder = 0),
-      easyClose = TRUE,
-      footer = NULL
-    ))
+  output$startGameButton <- renderUI({
+    actionButton("startGameBtn", "Start Game")
   })
   
-  observeEvent(input$createdByButton, {
-    showModal(modalDialog(
-      title = "Created By",
-      tags$iframe(src = "createdby.html", width = "100%", height = "600px", frameborder = 0),
-      easyClose = TRUE,
-      footer = NULL
-    ))
+  observeEvent(input$startGameBtn, {
+    game_instance(Game$new(input$player1, input$player2))
+    game_vectors(GameVectors$new(input$player1, input$player2))
+  })
+  
+  output$addNodeInputs <- renderUI({
+    req(game_instance())
+    tagList(
+      textInput("rootNode", "Root Node:", value = "A"),
+      textInput("decision", "Decision:", value = ""),
+      checkboxInput("isLastNode", "Is Last Node?", value = FALSE),
+      numericInput("pointsPlayer1", "Points for Player 1:", value = 0),
+      numericInput("pointsPlayer2", "Points for Player 2:", value = 0)
+    )
+  })
+  
+  output$addNodeButton <- renderUI({
+    req(game_instance())
+    actionButton("addNodeBtn", "Add Node")
+  })
+  
+  observeEvent(input$addNodeBtn, {
+    req(game_instance())
+    root_node <- input$rootNode
+    decision <- input$decision
+    is_last_node <- input$isLastNode
+    points_player1 <- input$pointsPlayer1
+    points_player2 <- input$pointsPlayer2
+    
+    game_instance()$addDecisionAndEdge(root_node, decision, is_last_node, points_player1, points_player2)
+    game_vectors()$addDecisionAndEdge_Vector(root_node, decision, is_last_node, points_player1, points_player2)
+    
+    output$treePlot <- renderPlot({
+      game_instance()$plotTree()
+    })
+  })
+  
+  output$performBackwardInductionButton <- renderUI({
+    req(game_instance())
+    actionButton("performBackwardsInductionBtn", "Perform Backward Induction")
+  })
+  
+  observeEvent(input$performBackwardsInductionBtn, {
+    req(game_instance())
+    optimal_paths <- game_instance()$performBackwardInduction()
+    
+    output$optimalPaths <- renderText({
+      paste("Best path for", game_instance()$player1_name, ":", paste(optimal_paths$best_path_player1, collapse = " -> "), "\n",
+            "Max payoff for", game_instance()$player1_name, ":", optimal_paths$max_payoff_player1, "\n",
+            "Best path for", game_instance()$player2_name, ":", paste(optimal_paths$best_path_player2, collapse = " -> "), "\n",
+            "Max payoff for", game_instance()$player2_name, ":", optimal_paths$max_payoff_player2)
+    })
+  })
+  
+  output$errorMessage <- renderText({
+    req(error_message())
+    error_message()
   })
 }
 
